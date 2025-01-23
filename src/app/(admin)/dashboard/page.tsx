84d5a904144c38ef31dbcb3bb614a5966b6e6cb4
Willys Varela <willysvarela@gmail.com>
2025-01-23 17:30:17 -0400

    feat: view user photos on modal

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -9,6 +9,7 @@ import TeensAnimation from "@/components/layout/teensAnimation";
 import Image from "next/image";
 import { User } from "@prisma/client";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { UserPhotoDialog } from "@/components/blocks/user-photo-dialog";
 
 const DashboardPage = () => {
     const [search, setSearch] = useState("");
@@ -72,25 +73,7 @@ const DashboardPage = () => {
                     {filteredTeens.map((teen) => (
                         <TableRow key={teen.id}>
                             <TableCell>
-                                <Dialog>
-                                    <DialogTrigger>
-                                        <div className="flex flex-row items-center justify-center gap-4">
-                                            <Image src={teen.teenPhoto} alt={teen.name} className="w-10 h-10 rounded-full" width={100} height={100} />
-                                            <span className="text-sm text-muted-foreground">Ver Fotos</span>
-                                        </div>
-                                    </DialogTrigger>
-                                    <DialogContent>
-                                        <DialogHeader>
-                                            <DialogTitle>{teen.name}</DialogTitle>
-                                        </DialogHeader>
-                                        <DialogDescription>
-                                            <div className="flex flex-row gap-2">
-                                                <Image src={teen.teenPhoto} alt={teen.name} className="w-full h-full rounded-sm" width={300} height={300} />
-                                                <Image src={teen.familyPhoto} alt={teen.name} className="w-full h-full rounded-sm" width={300} height={300} />
-                                            </div>
-                                        </DialogDescription>
-                                    </DialogContent>
-                                </Dialog>
+                                <UserPhotoDialog teen={teen} />
                             </TableCell>
 
                             <TableCell>{teen.name}</TableCell>
@@ -126,7 +109,7 @@ const DashboardPage = () => {
                 {filteredTeens.map((teen) => (
                     <Card key={teen.id} className="mb-4">
                         <CardHeader className="flex flex-row items-center gap-4">
-                            <Image src={teen.teenPhoto} alt={teen.name} className="w-10 h-10 rounded-full" width={100} height={100} />
+                            <UserPhotoDialog teen={teen} />
                             <div>
                                 <CardTitle>{teen.name}</CardTitle>
                                 <p className="text-sm text-muted-foreground">{teen.department}</p>
---
+++ src/components/blocks/user-photo-dialog.tsx
@@ -0,0 +1,28 @@
+import { Dialog, DialogHeader, DialogTrigger, DialogContent, DialogTitle, DialogDescription } from "@/components/ui/dialog"
+import { User } from "@prisma/client"
+import Image from "next/image"
+import React from "react"
+
+export const UserPhotoDialog: React.FC<{ teen: Partial<User> }> = ({ teen }) => {
+    return (
+        <Dialog>
+            <DialogTrigger>
+                <div className="flex flex-col md:flex-row items-center justify-center gap-4">
+                    <Image src={teen.teenPhoto || ""} alt={teen.name || ""} className="w-10 h-10 rounded-full" width={100} height={100} />
+                    <span className="text-sm text-muted-foreground">Ver Fotos</span>
+                </div>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{teen.name}</DialogTitle>
+                </DialogHeader>
+                <DialogDescription>
+                    <div className="flex flex-row gap-2">
+                        <Image src={teen.teenPhoto || ""} alt={teen.name || ""} className="w-full h-full rounded-sm" width={300} height={300} />
+                        <Image src={teen.familyPhoto || ""} alt={teen.name || ""} className="w-full h-full rounded-sm" width={300} height={300} />
+                    </div>
+                </DialogDescription>
+            </DialogContent>
+        </Dialog>
+    )
+}
