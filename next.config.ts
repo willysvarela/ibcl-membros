51e4f9f778a6ae93809ad9ea73afee835e1f9de3
Willys Varela <willysvarela@gmail.com>
2025-01-23 11:29:02 -0400

    improv: upgrade bodySizeLimit from 8 to 12mb

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     serverActions: {
-        bodySizeLimit: "8mb",
+        bodySizeLimit: "12mb",
     },
   },
 };
